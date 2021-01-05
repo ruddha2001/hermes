4618f3a3ee3a1dd0f930e55aa734646c57d55553
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-06 03:22:34 +0530

    :green_heart: Fix invalid IV length

--- src/api/User/encryptionService.ts
+++ src/api/User/encryptionService.ts
@@ -3,17 +3,17 @@ const algorithm = "aes-256-cbc";
 const key = process.env.CRYPTO_KEY!;
 
 export const encrypt = (rawData: string) => {
-  let iv = randomBytes(16);
+  let iv = randomBytes(20).toString("hex").slice(0, 16);
   let cipher = createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(rawData);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
-  return { iv: iv.toString("hex"), encryptedData: encrypted.toString("hex") };
+  console.log(iv);
+  return { iv: iv, encryptedData: encrypted.toString("hex") };
 };
 
 export const decrypt = (encryptedData: string, incomingIv: string) => {
-  let iv = Buffer.from(incomingIv, "hex");
   let encryptedText = Buffer.from(encryptedData, "hex");
-  let decipher = createDecipheriv("aes-256-cbc", Buffer.from(key), iv);
+  let decipher = createDecipheriv("aes-256-cbc", Buffer.from(key), incomingIv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
